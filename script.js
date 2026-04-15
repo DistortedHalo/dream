0aba76236fcfcd70b97c80ba59de4d0f23237331
Mik <mikzerafa2@hotmail.com>
2026-04-15 11:03:06 +0200

    added sound presets

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1URf5tU5YpfFCoejFca1NhBe2ZeFQPySx"; 
+const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1qGYgNQ2tKqq3eTr9QWIcXKXWvSi7xvNz"; 
 const button = document.getElementById("downloadButton");
 if (button) {
   button.href = DOWNLOAD_URL;
