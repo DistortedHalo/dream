897da2364cfce551b95e8492277a844f40c11adc
Mik <mikzerafa2@hotmail.com>
2026-04-15 09:31:09 +0200

    changed target of .exe

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=";
+const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1iMgbw450Vm6PC-h19sCmnoNCbIr5JO1J";
 const button = document.getElementById("downloadButton");
 if (button) {
   button.href = DOWNLOAD_URL;
