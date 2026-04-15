105fe43577430a49d709a5b213e362ad637ec6d6
Mik <mikzerafa2@hotmail.com>
2026-04-16 00:51:14 +0200

    changed site location

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1ogaOEjomRFn95gsBidlLDEZ275dC2Rko";
+const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1Vpf5iv1pTTTsNRXDt9v8QJ8Kd5tM_eIr";
 const button = document.getElementById("downloadButton");
 if (button) {
   button.href = DOWNLOAD_URL;
