c6bfcafd8ed99357e1fb97078299eaacdf5a126a
Mik <mikzerafa2@hotmail.com>
2026-04-15 20:24:00 +0200

    yelo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-
 const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1ogaOEjomRFn95gsBidlLDEZ275dC2Rko";
+const button = document.getElementById("downloadButton");
 if (button) {
   button.href = DOWNLOAD_URL;
 }
