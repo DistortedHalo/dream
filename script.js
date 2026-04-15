520e918c3e42f07cc3fd8fb71a02eb6cc1dd42d3
Mik <mikzerafa2@hotmail.com>
2026-04-15 08:29:11 +0200

    temp down

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1F5Ped5jvofvtOooMGrW8euLASROisNr6";
+const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=";
 const button = document.getElementById("downloadButton");
 if (button) {
   button.href = DOWNLOAD_URL;
