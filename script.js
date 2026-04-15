0cfef4e7a047fc9d0dec669f647b6a8c93abee53
Mik <mikzerafa2@hotmail.com>
2026-04-15 08:27:08 +0200

    fixes;

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/next"
 
 const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1F5Ped5jvofvtOooMGrW8euLASROisNr6";
 const button = document.getElementById("downloadButton");
