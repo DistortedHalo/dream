b47f48215a85867c783c6838f089776aad5e0cb0
Mik <mikzerafa2@hotmail.com>
2026-04-15 20:22:45 +0200

    fixed link to zip

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
 
-const getAnalytics = () => {
-  return <Analytics />;
-};
 
 const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1ogaOEjomRFn95gsBidlLDEZ275dC2Rko";
 if (button) {
