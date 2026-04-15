edb06ac54c981c209f660ba9b4bd79ffc5721025
Mik <mikzerafa2@hotmail.com>
2026-04-15 08:25:50 +0200

    added looper, sample library, drag and drop samples to instruments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { Analytics } from "@vercel/analytics/next"
 
-const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1c0g9OpI7mQuXdoqasQnbNrtVBuLGMAzb";
+const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1F5Ped5jvofvtOooMGrW8euLASROisNr6";
 const button = document.getElementById("downloadButton");
 if (button) {
   button.href = DOWNLOAD_URL;
