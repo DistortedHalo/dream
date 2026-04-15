734b5150fc63a4b599abb39b554bf94d581656a5
Mik <mikzerafa2@hotmail.com>
2026-04-15 17:51:23 +0200

    added analytics;

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+import { Analytics } from "@vercel/analytics/next";
+
+const getAnalytics = () => {
+  return <Analytics />;
+};
 
 const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1-Cm1_AFMAnzKp6cw2dtkB0OplDouXb55";
 const button = document.getElementById("downloadButton");
