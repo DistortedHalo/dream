00867c8f41fc7db08d38fc4e25aba82678fd7273
Mik <mikzerafa2@hotmail.com>
2026-04-15 05:37:25 +0200

    added analytics

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+
 const DOWNLOAD_URL = "https://drive.google.com/uc?export=download&id=1c0g9OpI7mQuXdoqasQnbNrtVBuLGMAzb";
 const button = document.getElementById("downloadButton");
 if (button) {
